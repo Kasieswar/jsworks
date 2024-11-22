db42756f83e2dbb44a3ac18621223e19d8ac125f
Kasieswar <kasieswar26@gmail.com>
2024-11-22 13:08:52 +0530

    first commit

---
+++ accounts/account.js
@@ -0,0 +1,53 @@
+function acc() {
+    let value = +prompt("Enter 4 digits Pin   hint: 9032")
+    const pin = 9032
+    
+    const valid = value === pin
+
+    if (valid) {
+        alert("Access granted.");
+        window.location.href="accountdet.html"
+    } 
+    else {
+        alert("Incorrect Pin. Please try again.");
+
+        
+    }
+    
+}
+
+function acc1() {
+    let value = +prompt("Enter 4 digits Pin   hint: 9032")
+    const pin = 9032
+    
+    const valid = value === pin
+
+    if (valid) {
+        alert("Access granted.");
+        window.location.href="accountdet.html"
+    } 
+    else {
+        alert("Incorrect Pin. Please try again.");
+
+        
+    }
+    
+}
+
+function acc2() {
+    let value = +prompt("Enter 4 digits Pin   hint: 9032")
+    const pin = 9032
+    
+    const valid = value === pin
+
+    if (valid) {
+        alert("Access granted.");
+        window.location.href="accountdet.html"
+    } 
+    else {
+        alert("Incorrect Pin. Please try again.");
+
+        
+    }
+    
+}
