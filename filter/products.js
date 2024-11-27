999961142a0b6fb52c442445f258cbf2d33f170a
Kasieswar <kasieswar26@gmail.com>
2024-11-27 12:45:11 +0530

    carts

--- filter/products.js
+++ filter/products.js
@@ -1,102 +1,101 @@
 // Array of products
 const products = [
     {
-        title: "Majestic Man Cotton Checkered Slim Fit Casual Shirt",
-        image: "https://m.media-amazon.com/images/I/71lXmdn59hL._SY741_.jpg",
-        material: "Pure Cotton",
-        pattern: "Checkered",
-        fit: "Slim Fit",
-        price: 498
+        title: "Espoir Black DIAL and Silver Strap Watch",
+        image: "https://m.media-amazon.com/images/I/71+EXKho4VS._SX679_.jpg",
+        Casediameter: "38.5 Millimetres",
+        Bandcolour :"Silver",
+        Warranty: "typeManufacturer",
+        price: 359
     },
     {
-        title: "Symbol Men's Solid Cotton Shirt | Casual | Plain | Full Sleeve",
-        image: "https://m.media-amazon.com/images/I/71y0jvWeG3L._SY879_.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
+        title: "Espoir Stainless Steel Analog Mens Watch",
+        image: "https://m.media-amazon.com/images/I/71QoQPgheFL._SX679_.jpg",
+        Casediameter: "40 Millimetres",
+        Bandcolour: "Silver",
+        Warrantytype :"Manufacturer",
+        price: 299
+    },
+    {
+        title: "LOUIS DEVIN WT030 Leather Strap Analog Wrist Watch for Men",
+        image: "https://m.media-amazon.com/images/I/81kLhCWFSjL._SX679_.jpg",
+        Casediameter: "45 Millimetres",
+        Bandcolour: "Silver",
+        Warrantytype :"Manufacturer",
+        price: 379
+    },
+    {
+        title: "Casio Vintage A158WA-1DF Black Digital Dial Silver Stainless Steel Band D011",
+        image: "https://m.media-amazon.com/images/I/61ybeKQto8L._SY679_.jpg",
+        Casediameter: "45 Millimetres",
+        Bandcolour: "Silver",
+        Warrantytype :"Manufacturer",
+        price: 459
+    },
+    {
+        title: "SELLORIA Black Dial Analogue Stainless Still Black Dial Analog Watch",
+        image: "https://m.media-amazon.com/images/I/41D0W0JLCIL.jpg",
+        Casediameter: "45 Millimetres",
+        Bandcolour: "Black",
+        Warrantytype :"No Warranty",
+        price: 199
+    },
+    {
+        title: "Titan Quartz Analog Silver Dial Leather Strap Watch for Women",
+        image: "https://m.media-amazon.com/images/I/61Zd0Uuw0uL._SX679_.jpg",
+        Casediameter: "27 Millimetres",
+        Bandcolour: "Brown",
+        Warrantytype :"Manufacturer",
+        price: 1995
+    },
+    {
+        title: "Matrix Minimalist Dial with Softest Silicone Strap Analog Wrist Watch",
+        image: "https://m.media-amazon.com/images/I/618MdeNFBmL._SX679_.jpg",
+        Casediameter: "57 Millimetres",
+        Bandcolour: "57",
+        Warrantytype :"Manufacturer",
         price: 549
     },
     {
-        title: "Majestic Man Slim Fit Satin Cotton Formal Shirt for Men",
-        image: "https://m.media-amazon.com/images/I/61HoVwwzRiL._SY879_.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
+        title: "SELLORIA Silicone Digital Watch Shockproof Multi-Functional Automatic Black Color Strap",
+        image: "https://m.media-amazon.com/images/I/61YTgYOiedL._SY679_.jpg",
+        Casediameter: "28 Millimetres",
+        Bandcolour: "Black",
+        Warrantytype :"No Warranty",
+        price: 169
     },
     {
-        title: "Dennis Lingo Men's Denim Solid Casual Shirt",
-        image: "https://m.media-amazon.com/images/I/61Z4RuwOZeL._SX679_.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
+        title: "Matrix Antique 2.0 Day & Date Softest Silicone Strap Analog Watch for Men",
+        image: "https://m.media-amazon.com/images/I/711NXCmUfbL._SX679_.jpg",
+        Casediameter: "44 Millimetres",
+        Bandcolour: "Black",
+        Warrantytype :"Manufacturer",
+        price: 318
     },
     {
-        title: "HKV-100 Men Striped Casual Shirt",
-        image: "https://m.media-amazon.com/images/I/41PskqEODBL.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
+        title: "Fastrack Analog Unisex-Adult Watch",
+        image: "https://m.media-amazon.com/images/I/61FFBTzKiUL._SX679_.jpg",
+        Casediameter: "57 Millimetres",
+        Bandcolour: "Black",
+        Warrantytype :"Manufacturer",
+        price: 809
     },
     {
-        title: "IndoPrimo Men's Casual Solid Satin Shirt for Men Full Sleeves - Crystal",
-        image: "https://m.media-amazon.com/images/I/71YpLqxFChL._SY879_.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
+        title: "NIBOSI Watches Men Sport Quartz Watches",
+        image: "https://m.media-amazon.com/images/I/516QZXGuq+L._SY679_.jpg",
+        Casediameter: "43 Millimetres",
+        Bandcolour: "Black",
+        Warrantytype :"Manufacturer",
+        price: 1990
     },
     {
-        title: "Parx Men Slim Fit Print Pattern Pure Cotton Semi Cutaway Collar Full Sleeve Casual Shirt",
-        image: "",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
+        title: "Titan Karishma Analog Black Dial Men's Watch",
+        image: "https://m.media-amazon.com/images/I/51ykbSj-eoL._SX679_.jpg",
+        Casediameter: "45.6 Millimetres",
+        Bandcolour: "Silver",
+        Warrantytype :"Manufacturer",
+        price: 1995
     },
-    {
-        title: "IndoPrimo Men's Solid Regular Fit Shirt",
-        image: "https://img.freepik.com/free-photo/handsome-model-man-posing_144627-42751.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
-    },
-    {
-        title: "Urbano Fashion Men's Full Sleeve Regular Fit Casual Denim Shirt",
-        image: "https://img.freepik.com/free-photo/handsome-model-man-posing_144627-42751.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
-    },
-    {
-        title: "Jack & Jones Men's Slim Fit Shirt",
-        image: "https://img.freepik.com/free-photo/handsome-model-man-posing_144627-42751.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
-    },
-    {
-        title: "Lymio Casual Shirt for Men|| Shirt for Men|| Men Stylish Shirt || Men Printed Shirt",
-        image: "https://img.freepik.com/free-photo/handsome-model-man-posing_144627-42751.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
-    },
-    {
-        title: "Park Avenue Men's Polyester Blend Semi Cutaway Collar Full Sleeve Formal Shirt",
-        image: "https://img.freepik.com/free-photo/handsome-model-man-posing_144627-42751.jpg",
-        material: "100% Cotton",
-        pattern: "Solid",
-        fit: "Regular Fit",
-        price: 549
-    },
-    // Add more products here
 ];
 
 // Function to render products
@@ -106,14 +105,14 @@ function renderProducts() {
 
     products.forEach((product, index) => {
         const productCard = `
-            <div class="col-md-4">
-                <div class="card mb-4">
+            <div class="col-md-3">
+                <div class="card">
                     <img src="${product.image}" class="card-img-top" alt="${product.title}">
                     <div class="card-body">
                         <h5 class="card-title">${product.title}</h5>
-                        <p class="card-text">Material: ${product.material}</p>
-                        <p class="card-text">Pattern: ${product.pattern}</p>
-                        <p class="card-text">Fit: ${product.fit}</p>
+                        <p class="card-text">Material: ${product.Casediameter}</p>
+                        <p class="card-text">Pattern: ${product.Bandcolour}</p>
+                        <p class="card-text">Fit: ${product.Warrantytype}</p>
                         <p class="card-text">Price: ₹${product.price}</p>
                         <button class="btn btn-primary" onclick="addToCart(${index})">Add to Cart</button>
                     </div>
